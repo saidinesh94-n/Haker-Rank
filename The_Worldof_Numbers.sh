a3e57dbd57c5597af23266b8108b713b15af81b7
saidinesh94-n <saianchela@gmail.com>
2022-02-04 11:39:27 +0530

    The World of Numbers

---
+++ The_Worldof_Numbers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Given two integers, X and Y, find their sum, difference, product, and quotient.
+
+x=5
+y=2
+echo $(( x + y ))
+echo $(( x - y ))
+echo $(( x * y ))
+echo $(( x / y ))
+
